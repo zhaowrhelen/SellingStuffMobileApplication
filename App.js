9c46522b7b3253c8a16b7cd6d8cec828c9a4ac1c
zhaowrhelen <zhaowrhelen@gmail.com>
2023-05-24 17:09:53 -0700

    add image

--- App.js
+++ App.js
@@ -1,14 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView,Image } from 'react-native';
 
 export default function App() {
 
   console.log("app executed")
+
+  const handlePress = () => console.log("Text pressed");
+
   return (
-    <View style={styles.container}>
-      <Text>hello World!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text onPress={handlePress}>hello World!</Text>
+      {/* <Image source = {require("./assets/splash.png")}/> */}
+      <Image source = {{
+        width:200,
+        height:300,
+      uri:"https://picsum.photos/200/300",}}/>
+      {/* <StatusBar style="auto" /> */}
+    </SafeAreaView>
   );
 }
 
