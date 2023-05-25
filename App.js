359720ab0152f3984eef9d7d60cb7d05ec5aa416
zhaowrhelen <zhaowrhelen@gmail.com>
2023-05-24 18:04:56 -0700

    Update App.js

--- App.js
+++ App.js
@@ -7,6 +7,8 @@ import {
   Image,
   TouchableWithoutFeedback,
   TouchableOpacity,
+  Button,
+  Alert,
 } from "react-native";
 
 export default function App() {
@@ -14,11 +16,16 @@ export default function App() {
 
   const handlePress = () => console.log("Text pressed");
   const handleTouch = () => console.log("image touch");
+  const buttonClick = () =>
+    Alert.alert("title", "message", [
+      { text: "yes", onPress: () => console.log("yes") },
+      { text: "no", onPress: () => console.log("no") },
+    ]);
 
   return (
     <SafeAreaView style={styles.container}>
       <Text onPress={handlePress}>hello World!</Text>
-      {/* <Image source = {require("./assets/splash.png")}/> */}
+      {/* <Image source={require("./assets/splash.png")} /> */}
       <TouchableOpacity onPress={handleTouch}>
         <Image
           source={{
@@ -28,6 +35,8 @@ export default function App() {
           }}
         />
       </TouchableOpacity>
+
+      <Button title="Click Me" onPress={buttonClick} />
 
       {/* <StatusBar style="auto" /> */}
     </SafeAreaView>
