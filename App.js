64b6eefc5d1a0cefe44dbfdbf79d5ad24d7e3992
zhaowrhelen <zhaowrhelen@gmail.com>
2023-05-30 16:54:06 -0700

    welcome page improve

--- App.js
+++ App.js
@@ -19,7 +19,7 @@ import WelcomeScreen from "./app/screens/WelcomeScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 
 export default function App() {
-  // return <WelcomeScreen />;
+  return <WelcomeScreen />;
 
-  return <ViewImageScreen />;
+  // return <ViewImageScreen />;
 }
--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,19 +1,29 @@
 import React from "react";
 import { Image, ImageBackground, StyleSheet, Text, View } from "react-native";
+import AppButton from "../components/AppButton";
 
 function WelcomeScreen(props) {
   return (
     <ImageBackground
+      blurRadius={10}
       style={styles.background}
       source={require("../assets/background.jpg")}
     >
       <View style={styles.logoContainer}>
         <Image style={styles.logo} source={require("../assets/logo-red.png")} />
-        <Text>Sell What You Don't Need</Text>
+        <Text style={styles.tagline}>Sell What You Don't Need</Text>
       </View>
-
-      <View style={styles.loginButton}></View>
-      <View style={styles.registerButton}></View>
+      <View style={styles.buttonsContainer}>
+        <AppButton
+          title="Login"
+          onPress={() => console.log("press login button")}
+        ></AppButton>
+        <AppButton
+          title="Register"
+          color="secondary"
+          onPress={() => console.log("press register button")}
+        ></AppButton>
+      </View>
     </ImageBackground>
   );
 }
@@ -24,16 +34,11 @@ const styles = StyleSheet.create({
     justifyContent: "flex-end",
     alignItems: "center",
   },
-  loginButton: {
+  buttonsContainer: {
+    padding: 20,
     width: "100%",
-    height: 70,
-    backgroundColor: "#fc5c65",
   },
-  registerButton: {
-    width: "100%",
-    height: 70,
-    backgroundColor: "#4ecdc4",
-  },
+
   logo: {
     width: 100,
     height: 100,
@@ -42,6 +47,11 @@ const styles = StyleSheet.create({
     position: "absolute",
     top: 70,
     alignItems: "center",
+  },
+  tagline: {
+    fontSize: 25,
+    fontWeight: "600",
+    paddingVertical: 20,
   },
 });
 
