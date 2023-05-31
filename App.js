1429cf3e87d329a3d37c55e2e27eb6ccf1de0970
zhaowrhelen <zhaowrhelen@gmail.com>
2023-05-31 16:04:20 -0700

    screen and list component

--- App.js
+++ App.js
@@ -18,11 +18,13 @@ import {
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
+import MessageScreen from "./app/screens/MessageScreen";
 
 export default function App() {
   // return <WelcomeScreen />;
 
   // return <ListingDetailsScreen />;
 
-  return <ViewImageScreen />;
+  // return <ViewImageScreen />;
+  return <MessageScreen></MessageScreen>;
 }
---
+++ app/screens/MessageScreen.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { FlatList, StyleSheet, View } from "react-native";
+import ListItem from "../components/ListItem";
+
+import Screen from "../components/Screen";
+
+// console.log(Constants);
+
+const message = [
+  {
+    id: 1,
+    title: "T1",
+    description: "D1",
+    image: require("../assets/profile-img.jpg"),
+  },
+  {
+    id: 2,
+    title: "T2",
+    description: "D2",
+    image: require("../assets/profile-img.jpg"),
+  },
+];
+
+function MessageScreen(props) {
+  return (
+    <Screen>
+      <FlatList
+        data={message}
+        keyExtractor={(message) => message.id.toString()}
+        renderItem={({ item }) => (
+          <ListItem
+            title={item.title}
+            subTitle={item.description}
+            image={item.image}
+          ></ListItem>
+        )}
+      ></FlatList>
+    </Screen>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default MessageScreen;
