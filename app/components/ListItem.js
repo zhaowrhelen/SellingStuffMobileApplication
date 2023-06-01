fc2e373ad7c6441601c7cc3df3187a87aae0f9ce
zhaowrhelen <zhaowrhelen@gmail.com>
2023-05-31 17:33:05 -0700

    delete

--- app/components/ListItem.js
+++ app/components/ListItem.js
@@ -1,22 +1,28 @@
 import React from "react";
-import { View, StyleSheet, Image } from "react-native";
+import { View, StyleSheet, Image, TouchableHighlight } from "react-native";
 import AppText from "./AppText";
 import colors from "../config/colors";
+import Swipeable from "react-native-gesture-handler/Swipeable";
 
-function ListItem({ title, subTitle, image }) {
+function ListItem({ title, subTitle, image, onPress, renderRightActions }) {
   return (
-    <View style={styles.container}>
-      <Image style={styles.image} source={image}></Image>
-      <View>
-        <AppText style={styles.title}>{title}</AppText>
-        <AppText style={styles.subTitle}>{subTitle}</AppText>
-      </View>
-    </View>
+    <Swipeable renderRightActions={renderRightActions}>
+      <TouchableHighlight underlayColor={colors.light} onPress={onPress}>
+        <View style={styles.container}>
+          <Image style={styles.image} source={image}></Image>
+          <View>
+            <AppText style={styles.title}>{title}</AppText>
+            <AppText style={styles.subTitle}>{subTitle}</AppText>
+          </View>
+        </View>
+      </TouchableHighlight>
+    </Swipeable>
   );
 }
 const styles = StyleSheet.create({
   container: {
     flexDirection: "row",
+    padding: 15,
   },
   image: {
     width: 70,
--- app/screens/MessageScreen.js
+++ app/screens/MessageScreen.js
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { FlatList, StyleSheet, View } from "react-native";
 import ListItem from "../components/ListItem";
 
 import Screen from "../components/Screen";
+import colors from "../config/colors";
+import ListItemSeparator from "../components/ListItemSeparator";
+import ListItemDeleteAction from "../components/ListItemDeleteAction";
 
 // console.log(Constants);
 
-const message = [
+const initialMessages = [
   {
     id: 1,
     title: "T1",
@@ -22,18 +25,49 @@ const message = [
 ];
 
 function MessageScreen(props) {
+  // setState
+  //   const array = useState(0);
+  //   const count = array[0];
+  //   const setCount = array[1];
+
+  const [messages, setMessages] = useState(initialMessages);
+  const [refreshing, setRefreshing] = useState(false);
+
+  const handleDetele = (message) => {
+    // delete the message from messages
+    const newMessages = messages.filter((m) => m.id !== message.id);
+    setMessages(newMessages);
+    // call the server
+  };
+
   return (
     <Screen>
       <FlatList
-        data={message}
+        data={messages}
         keyExtractor={(message) => message.id.toString()}
         renderItem={({ item }) => (
           <ListItem
             title={item.title}
             subTitle={item.description}
             image={item.image}
+            onPress={() => console.log("message selected", item)}
+            renderRightActions={() => (
+              <ListItemDeleteAction onPress={() => handleDetele(item)} />
+            )}
           ></ListItem>
         )}
+        ItemSeparatorComponent={ListItemSeparator}
+        refreshing={refreshing}
+        onRefresh={() => {
+          setMessages([
+            {
+              id: 2,
+              title: "T2",
+              description: "D2",
+              image: require("../assets/profile-img.jpg"),
+            },
+          ]);
+        }}
       ></FlatList>
     </Screen>
   );
